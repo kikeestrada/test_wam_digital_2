0b24887b26f584d1d9511cc29aff85540d513652
kike <kike@MacBook-Pro-de-kike.local>
2021-06-14 15:54:46 -0600

    fisrt commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
+import {topNav} from './modules/topNav'
 import {swDetecter} from './modules/swDetecter';
 (()=>{
 	swDetecter();
+	topNav();
 	if (document.body.classList.contains('home')) {
 		// functions here
 	}else if (document.body.classList.contains('page2')) {
