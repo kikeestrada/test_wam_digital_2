86873e2bb0b74daf758d482dab2440232664a15d
kike <kike@MacBook-Pro-de-kike.local>
2021-06-14 22:16:05 -0600

    Updates

--- assets/js/scripts-min.js
+++ assets/js/scripts-min.js
@@ -1,2 +1,59 @@
-!function(){return function e(r,t,n){function o(s,c){if(!t[s]){if(!r[s]){var u="function"==typeof require&&require;if(!c&&u)return u(s,!0);if(i)return i(s,!0);var a=new Error("Cannot find module '"+s+"'");throw a.code="MODULE_NOT_FOUND",a}var f=t[s]={exports:{}};r[s][0].call(f.exports,function(e){return o(r[s][1][e]||e)},f,f.exports,e,r,t,n)}return t[s].exports}for(var i="function"==typeof require&&require,s=0;s<n.length;s++)o(n[s]);return o}}()({1:[function(e,r,t){"use strict";(0,e("./modules/swDetecter").swDetecter)(),document.body.classList.contains("home")||document.body.classList.contains("page2")||document.body.classList.contains("page3")},{"./modules/swDetecter":2}],2:[function(e,r,t){"use strict";Object.defineProperty(t,"__esModule",{value:!0});t.swDetecter=function(){"serviceWorker"in navigator&&navigator.serviceWorker.register("./sw.js").then(function(e){return console.log("Registro de SW exitoso",e)}).catch(function(e){return console.warn("Error al tratar de registrar el sw",e)})}},{}]},{},[1]);
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+'use strict';
+
+var _topNav = require('./modules/topNav');
+
+var _swDetecter = require('./modules/swDetecter');
+
+(function () {
+	(0, _swDetecter.swDetecter)();
+	(0, _topNav.topNav)();
+	if (document.body.classList.contains('home')) {
+		// functions here
+	} else if (document.body.classList.contains('page2')) {
+		// functions here
+	} else if (document.body.classList.contains('page3')) {
+		// functions here
+	}
+})();
+
+},{"./modules/swDetecter":2,"./modules/topNav":3}],2:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+var swDetecter = exports.swDetecter = function swDetecter() {
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('./sw.js').then(function (reg) {
+			return console.log('Registro de SW exitoso', reg);
+		}).catch(function (err) {
+			return console.warn('Error al tratar de registrar el sw', err);
+		});
+	}
+};
+
+},{}],3:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+var topNav = exports.topNav = function topNav() {
+	var myFunction = function myFunction() {
+		document.querySelector('.hamburger').addEventListener('click', function (e) {
+			e.preventDefault();
+			[].map.call(document.querySelectorAll('.hamburger'), function (el) {
+				el.classList.toggle('is-active');
+			});
+			[].map.call(document.querySelectorAll('.top-nav__menu'), function (el) {
+				el.classList.toggle('show-top-nav');
+			});
+		});
+	};
+	myFunction();
+};
+
+},{}]},{},[1]);
+
 //# sourceMappingURL=scripts-min.js.map
